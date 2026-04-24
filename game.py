8164e5a0e3ad0d7e36029174e446ec31d9e2d05f
Braeden Sy Tan <braedenjairsytan@icloud.com>
2026-04-25 09:10:40 +1200

    Added calss colors

--- game.py
+++ game.py
@@ -9,3 +9,29 @@ import sys
 import termios
 import tty
 
+#----Colours----#
+class Colours:
+    '''colours for texts'''
+    BLACK = '\033[38;5;0m'
+    RED = '\033[38;5;1m'
+    GREEN = '\033[38;5;2m'
+    YELLOW = '\033[38;5;3m'
+    BLUE = '\033[38;5;4m'
+    MAGENTA = '\033[38;5;5m'
+    CYAN = '\033[38;5;6m'
+    WHITE = '\033[38;5;7m'
+    GOLD = '\033[38;5;220m'
+    
+    BG_RED = '\033[48;5;196m'
+    BG_GREEN = '\033[48;5;46m'
+    BG_YELLOW = '\033[48;5;226m'
+    BG_BLUE = '\033[48;5;21m'
+    BG_WHITE = '\033[48;5;15m'
+    BG_BLACK = '\033[48;5;0m'
+
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ITALIC = '\033[3m'
+    
+    RESET = '\033[0m'
+#----Colours----#
