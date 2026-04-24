e012c5af80d4b1b5cb3040b931f20b6449a09ce1
Braeden Sy Tan <braedenjairsytan@icloud.com>
2026-04-25 09:12:48 +1200

    Added json encoder and decoded from previous game

--- game.py
+++ game.py
@@ -35,3 +35,48 @@ class Colours:
     
     RESET = '\033[0m'
 #----Colours----#
+
+#----Save File Money----#
+
+def to_binary_str(s):
+    '''binary encoder'''
+    return ''.join(format(ord(c), '08b') for c in s)
+
+def from_binary_str(b):
+    '''binary decoder'''
+    # Validate binary string
+    if len(b) % 8 != 0:
+        raise ValueError("Binary string length must be divisible by 8")
+    if not all(c in '01' for c in b):
+        raise ValueError("Binary string must only contain 0s and 1s")
+    
+    chars = [chr(int(b[i:i+8], 2)) for i in range(0, len(b), 8)]
+    return ''.join(chars)
+
+def encode_save(json_str):
+    '''encodes using method under'''
+    # Base64 encode
+    b64 = base64.b64encode(json_str.encode('utf-8')).decode('utf-8')
+    # Reverse
+    rev = b64[::-1]
+    # ROT13 encode
+    rot = codecs.encode(rev, 'rot_13')
+    # Binary encode
+    binary = to_binary_str(rot)
+    return binary.encode('utf-8')  # Write as bytes
+
+def decode_save(encoded_bytes):
+    '''decodes using method under'''
+    # grabs code
+    binary_str = encoded_bytes.decode('utf-8')
+    # Binary decode
+    rot = from_binary_str(binary_str)
+    # ROT13 decode
+    rev = codecs.decode(rot, 'rot_13')
+    # Reverse
+    b64 = rev[::-1]
+    # Base64 decode
+    json_str = base64.b64decode(b64).decode('utf-8')
+    return json_str
+
+#----Save File Money----#
